7772467fe6ea8c601ebb9a0788940c8019c85eb9
hyy <87626629+AmiraTS@users.noreply.github.com>
2022-11-26 21:43:48 +0800

    Add files via upload

---
+++ litemall-admin-api/src/main/java/org/linlinjava/litemall/admin/vo/OriginalVo.java
@@ -0,0 +1,51 @@
+package org.linlinjava.litemall.admin.vo;
+
+import java.util.List;
+
+public class OriginalVo {
+    private Integer ori_id;
+    private String oriUrl;
+    private Integer des_id;
+    private String desUrl;
+    private Integer user_id;
+
+    public Integer getOri_id() {
+        return ori_id;
+    }
+
+    public void setOri_id(Integer ori_id) {
+        this.ori_id = ori_id;
+    }
+
+    public String getOriUrl() {
+        return oriUrl;
+    }
+
+    public void setOriUrl(String oriUrl) {
+        this.oriUrl = oriUrl;
+    }
+
+    public Integer getDes_id() {
+        return des_id;
+    }
+
+    public void setDes_id(Integer des_id) {
+        this.des_id = des_id;
+    }
+
+    public String getDesUrl() {
+        return desUrl;
+    }
+
+    public void setDesUrl(String desUrl) {
+        this.desUrl = desUrl;
+    }
+
+    public Integer getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(Integer user_id) {
+        this.user_id = user_id;
+    }
+}
